1bb76c5e10685260dfd5982a73adb6d54df78fd9
hrero <279768876@qq.com>
2019-01-02 21:20:56 +0800

    no message

--- routes/company.js
+++ routes/company.js
@@ -4,37 +4,31 @@ const request = require('request'); // 处理node request请求
 const jwt = require('jsonwebtoken') // 生成签名token
 const allServices = require('../controllers/mysql') // 生成签名token
 const company = require('../controllers/store/company') // 生成签名token
-let responseData = {
-    code:0,
-    msg: '请求失败',
-    result: false,
-    data: ''
+
+function resData(obj) {
+    obj.code = obj.code || 0
+    obj.msg = obj.msg || '请求失败'
+    obj.result = obj.result || false
+    obj.data = obj.data || ''
+    return obj
 }
 router.prefix('/company')
 // 鄙视列表
 router.post('/getDissSelect', async (ctx, next) => {
     await allServices(company.dissListSql()).then((data) => {
-        responseData.data = data
-        responseData.msg = '请求成功'
-        responseData.result = true
-        ctx.body = responseData
+        ctx.body = resData({msg: '请求成功', result: true, data: data})
         next()
     }).catch(error => {
-        responseData.msg = '请求失败'
-        ctx.body = responseData
+        ctx.body = resData()
     })
 })
 // 排行列表
 router.post('/getRankingList', async (ctx, next) => {
     await allServices(company.rankingSql()).then((data) => {
-        responseData.data = data
-        responseData.msg = '请求成功'
-        responseData.result = true
-        ctx.body = responseData
+        ctx.body = resData({msg: '请求成功', result: true, data: data})
         next()
     }).catch(error => {
-        responseData.msg = '请求失败'
-        ctx.body = responseData
+        ctx.body = resData()
     })
 })
 // 添加接口
@@ -43,18 +37,13 @@ router.post('/addCompany', async (ctx, next) => {
     let data = await allServices(company.nameSql(req.name))
     if (data.length < 1) {
         await allServices(company.addSql(), [req.name,req.number,req.region,req.isGoOut,req.hopeGoOut,req.existence,req.diss,req.talk,req.star]).then((data) => {
-            responseData.data = ''
-            responseData.msg = '收到了,老板'
-            responseData.result = true
-            responseData.code = 1
-            ctx.body = responseData
+            ctx.body = resData({data: '', msg: '收到了,老板', result: true, code: 1})
             next()
         }).catch(error => {
             console.log(error)
         })
     } else {
-        responseData.msg = '我要被你点爆了'
-        ctx.body = responseData
+        ctx.body = resData({msg: '我要被你点爆了'})
     }
 })
 
@@ -76,21 +65,13 @@ router.post('/addStar', async (ctx, next) => {
     }
     let data = await isHaveClickStarFn(req.openid, req.id)
     if (data.length > 0) {
-        responseData.data = ''
-        responseData.msg = '您已经支持过一次了哦！'
-        responseData.result = true
-        responseData.code = 1
-        ctx.body = responseData
+        ctx.body = resData({data: '', msg: '您已经支持过一次了哦！', result: true, code: 1})
         next()
     } else {
         await allServices(company.addStarSql(req))
         await allServices(company.insertOpenidCompanyIdsQL(req)).then((err, data) => {
             if (data) {
-                responseData.data = ''
-                responseData.msg = '收到了,老板'
-                responseData.result = true
-                responseData.code = 1
-                ctx.body = responseData
+                ctx.body = resData({data: '', msg: '收到了,老板', result: true, code: 1})
             }
             if (err) {
                 console.log(err);
